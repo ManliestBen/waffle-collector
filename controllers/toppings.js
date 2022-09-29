9c1363302c04e09a56d51e35cacafd85c536cb9a
Ben Manley <benjamintmanley@gmail.com>
2022-09-28 19:24:53 -0500

    stub up controllers

---
+++ controllers/toppings.js
@@ -0,0 +1,7 @@
+import { Waffle } from "../models/waffle.js"
+import { Profile } from "../models/profile.js"
+import { Topping } from "../models/topping.js"
+
+export {
+  
+}
---
+++ controllers/waffles.js
@@ -0,0 +1,9 @@
+import { Waffle } from "../models/waffle.js"
+import { Profile } from "../models/profile.js"
+import { Topping } from "../models/topping.js"
+
+
+
+export {
+  
+}
