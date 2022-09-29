a15e44cad4adf139b5b8837299933f3793d33764
Ben Manley <benjamintmanley@gmail.com>
2022-09-28 19:21:29 -0500

    add topping and waffle models

---
+++ models/topping.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const toppingSchema = new mongoose.Schema({
+  name: String,
+}, {
+  timestamps: true
+})
+
+const Topping = mongoose.model('Topping', toppingSchema)
+
+export {
+  Topping
+}
---
+++ models/waffle.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const reviewSchema = new mongoose.Schema({
+  content: String,
+  reviewer: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile'}
+})
+
+const waffleSchema = new mongoose.Schema({
+  description: String,
+  owner: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile'},
+  reviews: [reviewSchema],
+  toppings: [{type: mongoose.Schema.Types.ObjectId, ref: 'Topping'}]
+}, {
+  timestamps: true
+})
+
+const Waffle = mongoose.model('Waffle', waffleSchema)
+
+export {
+  Waffle
+}
